1d5817988f91adcb6eaa7d607bd1e00d9a7aaa6f
Sam Yamashita <1587053+sotayamashita@users.noreply.github.com>
2019-02-11 22:23:34 +0900

    Update

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,7 @@ RUN mkdir -p ${APP_ROOT}
 WORKDIR ${APP_ROOT}
 
 COPY Gemfile* ${APP_ROOT}/
-RUN bundle install -j "$(getconf _NPROCESSORS_ONLN)" --retry 5 --no-document
+RUN bundle install -j "$(getconf _NPROCESSORS_ONLN)" --retry 5
 COPY . ${APP_ROOT}
 
 # Add a script to be executed every time the container starts.
--- rootfs/entrypoint-dev.sh
+++ rootfs/entrypoint-dev.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -eu
 
-# Load libraries
-. /opt/create-rails-app/base/functions
-
 bundle_check() {
   bundle check 1> /dev/null
 }
@@ -18,23 +15,29 @@ bunlde_install_if_need() {
   fi
 }
 
-if [[ "$1" == "bundle" ]] && [[ "$2" == "exec" ]] && [[ "$3" == "rails" ]] && [[ "$4" == "server" ]]; then
-  if [[ -f /app/config.ru ]]; then
-    log "Rails project found. Skipping creation..."
-  else
-    # Create a rails application
-    log "Creating new Ruby on Rails project..."
-    rails new . --force --skip-bundle --skip-test --skip-yarn --skip-coffee --database=postgresql
+# if [[ "$1" == "bundle" ]] && [[ "$2" == "exec" ]] && [[ "$3" == "rails" ]] && [[ "$4" == "server" ]]; then
+#   if [[ -f /app/config.ru ]]; then
+#     echo "Rails project found. Skipping creation..."
+#   else
+#     # Create a rails application
+#     echo "Creating new Ruby on Rails project..."
+#     rails new . --force --skip-bundle --skip-test --skip-yarn --skip-coffee --database=postgresql
+#
+#     echo "Configuring database.yml..."
+#     sed -i -e '1,/default:\  &default/ s/encoding:.*$/encoding:\ utf8/g' /app/config/database.yml
+#     sed -i -e '1,/default:\  &default/ s/encoding:.*/& \n\ \ host:\ db\n\ \ username:\ postgres\n\ \ password:/g' /app/config/database.yml
+#
+#     echo "Installing dependencies..."
+#     bundle_install
+#   fi
+#
+#   bunlde_install_if_need
+# fi
 
-    log "Configuring database.yml..."
-    sed -i -e '1,/default:\  &default/ s/encoding:.*$/encoding:\ utf8/g' /app/config/database.yml
-    sed -i -e '1,/default:\  &default/ s/encoding:.*/& \n\ \ host:\ db\n\ \ username:\ postgres\n\ \ password:/g' /app/config/database.yml
-
-    log "Installing dependencies..."
-    bundle_install
-  fi
-
-  bunlde_install_if_need
+# Remove a potentially pre-existing server.pid for Rails.
+if [ -f /app/tmp/pids/server.pid ]; then
+  rm -f /app/tmp/pids/server.pid
 fi
 
-exec tini -- "$@"
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"
